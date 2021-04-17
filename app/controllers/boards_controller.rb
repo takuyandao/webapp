ea2022694ddd43e18311b5758cca2d3f450f8876
takuyandao <takuyanmaru1212@gmail.com>
2021-04-17 20:14:24 +0900

    add-destroy-method

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,8 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-    @boards = Board.all
+    @boards = Board.page(params[:page])
   end
 
   def new
@@ -9,17 +10,36 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    board = Board.create(board_params)
+    redirect_to board
   end
 
   def show
-    @board = Board.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+
+    redirect_to @board
+  end
+
+  def destroy
+    @board.delete
+
+    redirect_to boards_path
   end
 
   private
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 
 end
